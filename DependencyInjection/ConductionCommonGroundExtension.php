ec0ff282ee8d5da6369e4c9dfc79479607d1d0b0
Robert Zondervan <Robert@conduction.nl>
2020-05-12 11:23:54 +0200

    Autowire CGS

---
+++ DependencyInjection/ConductionCommonGroundExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class ConductionCommonGroundExtension extends Extension
+{
+
+    public function load(array $configs, Container){
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
