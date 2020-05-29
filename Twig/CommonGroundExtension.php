cb1a357252af9e5eaf59a949bd6c1f1a7ae8f98e
Robert Zondervan <rjzondervan@gmail.com>
2020-05-29 13:05:52 +0200

    Add commonground_is_resource to twig

--- Twig/CommonGroundExtension.php
+++ Twig/CommonGroundExtension.php
@@ -15,6 +15,7 @@ class CommonGroundExtension extends AbstractExtension
             // the logic of this filter is now implemented in a different class
             new TwigFunction('commonground_resource_list', [CommonGroundRuntime::class, 'getResourceList']),
             new TwigFunction('commonground_resource', [CommonGroundRuntime::class, 'getResource']),
+            new TwigFunction('commonground_is_resource', [CommonGroundRuntime::class, 'isResource']),
             new TwigFunction('commonground_component_list', [CommonGroundRuntime::class, 'getComponentList']),
             new TwigFunction('commonground_component_health', [CommonGroundRuntime::class, 'getComponentHealth']),
             new TwigFunction('commonground_component_resources', [CommonGroundRuntime::class, 'getComponentResources']),
