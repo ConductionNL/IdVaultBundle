693422185586f7ff26f21eb15ac0e56ca00cd7d3
Robert Zondervan <Robert@conduction.nl>
2020-06-09 13:30:00 +0200

    prevent error

--- Service/CommonGroundService.php
+++ Service/CommonGroundService.php
@@ -497,7 +497,7 @@ class CommonGroundService
         $this->cache->save($item);
 
         // creates the ResourceUpdateEvent and dispatches it
-        $event = new CommongroundUpdateEvent($resource, $component);
+        $event = new CommongroundUpdateEvent($response, $component);
         $this->eventDispatcher->dispatch(
             $event,
             CommonGroundEvents::CREATED
