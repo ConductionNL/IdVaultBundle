00548580c03007f3de169e5e67583d7e73afbcc9
Ruben van der Linde <ruben@conduction.nl>
2020-06-25 22:06:49 +0200

    Hotfix on user id

--- Service/CommonGroundService.php
+++ Service/CommonGroundService.php
@@ -90,7 +90,7 @@ class CommonGroundService
             'Content-Crs'  => 'EPSG:4326',
         ];
 
-        if ($session->get('user')) {
+        if ($session->get('user') && array_key_exists('@id', $session->get('user'))) {
             $headers['X-NLX-Request-User-Id'] = $session->get('user')['@id'];
         }
 
