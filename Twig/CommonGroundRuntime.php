9ecfb625768d28dda4052b4be1278b912d342357
Robert Zondervan <Robert@conduction.nl>
2020-06-29 17:04:43 +0200

    also path

--- Twig/CommonGroundRuntime.php
+++ Twig/CommonGroundRuntime.php
@@ -61,9 +61,9 @@ class CommonGroundRuntime implements RuntimeExtensionInterface
     {
         return $this->commongroundService->cleanUrl($url, $resource, $autowire);
     }
-    public function getPath(string $route, array $route_parameters = [], bool $relative = false)
+    public function getPath(string $route, array $route_parameters = [], $relative = false)
     {
-        if($this->params->get('app_subpath') != 'false'){
+        if($this->params->get('app_subpath') && $this->params->get('app_subpath') != 'false'){
             return '/'.$this->params->get('app_subpath').$this->router->generate($route, $route_parameters, $relative);
         }
         else{
