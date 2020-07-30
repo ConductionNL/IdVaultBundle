9868a3585bc1ae1baf48cb5b48d6ccb46d5df556
Robert Zondervan <Robert@conduction.nl>
2020-07-30 10:25:41 +0200

    Fieldssubscriber on resource lists

--- Subscriber/FieldsAndExtendSubscriber.php
+++ Subscriber/FieldsAndExtendSubscriber.php
@@ -4,6 +4,7 @@ namespace Conduction\CommonGroundBundle\Subscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -72,50 +73,31 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
                 $contentType = 'application/ld+json';
                 $renderType = 'jsonld';
         }
+
         $json = $this->serializer->serialize(
             $result,
             $renderType,
             ['enable_max_depth' => true]
         );
+
         $array = json_decode($json, true);
-        if (!is_array($extends)) {
-            $extends = explode(',', $extends);
+        if(is_array(json_decode($json, false))){
+            foreach($array as $key=>$resource){
+                $array[$key] = $this->getExtends($extends, $resource);
+
+                if ($fields != [] && $fields != '') {
+                    $array[$key] = $this->getFields($fields, $resource);
+                }
+            }
         }
-        foreach ($extends as $extend) {
-            $extend = explode('.', $extend);
-            $array = $this->recursiveExtend($array, $extend);
+        else{
+            $array = $this->getExtends($extends, $array);
+            if ($fields != [] && $fields != '') {
+                $array = $this->getFields($fields, $array);
+            }
         }
 
         if ($fields != [] && $fields != '') {
-            // let turn fields into an array if it isn't one already
-            if (!is_array($fields)) {
-                $fields = explode(',', $fields);
-            }
-            // Its possible to nest fields for filterins
-            foreach ($fields as $key=>$field) {
-                $field = explode('.', $field);
-                unset($fields[$key]);
-                $field = $this->recursiveField($field);
-
-                $fields = $this->array_merge_recursive_ex($fields, $field);
-            }
-//            die;
-            // Overwrite maxdepth for extended properties
-            // we always need to return an id and links (in order not to break stuff)
-            if (!in_array('id', $fields)) {
-                $fields[] = 'id';
-            }
-            if (!in_array('@id', $fields)) {
-                $fields[] = '@id';
-            }
-            if (!in_array('@type', $fields)) {
-                $fields[] = '@type';
-            }
-            if (!in_array('@context', $fields)) {
-                $fields[] = '@context';
-            }
-            $array = $this->selectFields($array, $fields);
-
             // now we need to overide the normal subscriber
             $json = $this->serializer->serialize(
                 $array,
@@ -138,6 +120,47 @@ class FieldsAndExtendSubscriber implements EventSubscriberInterface
         );
         $this->commonGroundService->setHeader('Authorization', $this->params->get('app_commonground_key'));
         $event->setResponse($response);
+    }
+
+    public function getExtends($extends, $array){
+        if (!is_array($extends)) {
+            $extends = explode(',', $extends);
+        }
+        foreach ($extends as $extend) {
+            $extend = explode('.', $extend);
+            $array = $this->recursiveExtend($array, $extend);
+        }
+
+        return $array;
+    }
+    public function getFields($fields, $array){
+        if (!is_array($fields)) {
+            $fields = explode(',', $fields);
+        }
+        // Its possible to nest fields for filterins
+        foreach ($fields as $key=>$field) {
+            $field = explode('.', $field);
+            unset($fields[$key]);
+            $field = $this->recursiveField($field);
+
+            $fields = $this->array_merge_recursive_ex($fields, $field);
+        }
+//            die;
+        // Overwrite maxdepth for extended properties
+        // we always need to return an id and links (in order not to break stuff)
+        if (!in_array('id', $fields)) {
+            $fields[] = 'id';
+        }
+        if (!in_array('@id', $fields)) {
+            $fields[] = '@id';
+        }
+        if (!in_array('@type', $fields)) {
+            $fields[] = '@type';
+        }
+        if (!in_array('@context', $fields)) {
+            $fields[] = '@context';
+        }
+        return $this->selectFields($array, $fields);
     }
 
     public function recursiveExtend(array $resource, array $extend)
