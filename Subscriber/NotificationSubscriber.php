1d410b68b2209839e21d4989746c8aab8b70ab23
Ruben van der Linde <ruben@conduction.nl>
2020-09-01 08:58:51 +0200

    Added notification subscriber

---
+++ Subscriber/NotificationSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Conduction\CommonGroundBundle\Subscriber;
+
+use Conduction\CommonGroundBundle\Event\CommonGroundEvents;
+use Conduction\CommonGroundBundle\Event\CommongroundUpdateEvent;
+use Conduction\CommonGroundBundle\Service\VsbeService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class NotificationSubscriber implements EventSubscriberInterface
+{
+    private $notificationService;
+
+    public function __construct(notificationService $notificationService)
+    {
+        $this->notificationService = $notificationService;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CommonGroundEvents::DELETED  => 'deleted',
+            CommonGroundEvents::CREATED  => 'created',
+            CommonGroundEvents::UPDATED  => 'updated',
+        ];
+    }
+
+    // Our resource might reqoure aditional resources to be created, so lets look into that
+    public function deleted(CommongroundUpdateEvent $event)
+    {
+        $this->notificationService->notify($event->getResource(), 'deleted');
+        return $event;
+    }
+    // Our resource might reqoure aditional resources to be created, so lets look into that
+    public function updated(CommongroundUpdateEvent $event)
+    {
+        $this->notificationService->notify($event->getResource(), 'updated');
+
+        return $event;
+    }
+
+    // Our resource might reqoure aditional resources to be created, so lets look into that
+    public function created(CommongroundUpdateEvent $event)
+    {
+        $this->notificationService->notify($event->getResource(), 'created');
+
+        return $event;
+    }
+}
