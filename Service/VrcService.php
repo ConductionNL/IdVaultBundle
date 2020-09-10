1af2e12a58e934e28d787af3cbcd8e8b520d48bf
Gino <ginokok1996@hotmail.com>
2020-09-10 14:42:36 +0200

    fixes

--- Service/VrcService.php
+++ Service/VrcService.php
@@ -459,17 +459,14 @@ class VrcService
 
         $currentStage['orderNumber'] = null;
 
-        if (isset($property['properties']['stageNumber'])) {
-            var_dump($property['properties']);
-            exit;
-        }
-
-        if (isset($request['currentStage'])) {
+        if (isset($request['currentStage'])){
             if (filter_var($request['currentStage'], FILTER_VALIDATE_URL)) {
                 $currentStage = $this->commonGroundService->getResource($request['currentStage']);
-            } else {
-                $currentStage = $this->commonGroundService->getResourceList(['component' => 'ptc', 'type' => 'stages'], ['name' => ucfirst($request['currentStage'])])['hydra:member'];
-                $currentStage = $currentStage[0];
+            }else{
+                $currentStage = $this->commonGroundService->getResourceList(['component' => 'ptc', 'type' => 'stages'],['name' => ucfirst($request['currentStage'])])['hydra:member'];
+                if(isset($currentStage[0])){
+                    $currentStage = $currentStage[0];
+                }
             }
         }
 
@@ -479,7 +476,8 @@ class VrcService
             $result['valid'] = false;
 
             return $result;
-        } elseif ((!array_key_exists($property['name'], $request['properties'])) && $property['required']) {
+        }
+        elseif ((!array_key_exists($property['name'], $request['properties'])) && $property['required']) {
             $result['messages'] = ['value is required'];
             $result['valid'] = false;
 
