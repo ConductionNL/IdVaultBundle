2e3dcacb5ad94f7e53b021b7dd234a59e4f6fa81
Robert Zondervan <robert@conduction.nl>
2020-09-15 16:24:12 +0200

    ||

--- Service/CommonGroundService.php
+++ Service/CommonGroundService.php
@@ -1096,7 +1096,7 @@ class CommonGroundService
             // Lets make sure we dont have doubles
             $url = str_replace($this->params->get('app_env').'.', '', $url);
 
-            if (!$this->params->get('app_subpath_routing') && $this->params->get('app_subpath_routing') == 'false') {
+            if (!$this->params->get('app_subpath_routing') || $this->params->get('app_subpath_routing') == 'false') {
                 // e.g https://wrc.larping.eu/ becomes https://wrc.dev.larping.eu/
                 $host = explode('.', $parsedUrl['host']);
                 $subdomain = $host[0];
