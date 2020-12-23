16853a23a810bee12afb68380381299f2db4a028
ginokok1996 <ginokok1996@hotmail.com>
2020-12-23 09:43:34 +0100

    id-vault library rework

--- Service/IdVaultService.php
+++ Service/IdVaultService.php
@@ -3,71 +3,39 @@
 // Conduction/CommonGroundBundle/Service/IdVaultService.php
 namespace Conduction\IdVaultBundle\Service;
 
-use Conduction\CommonGroundBundle\Service\CommonGroundService;
+use Conduction\IdVaultApi\IdVaultApiClient;
 use GuzzleHttp\Client;
 use Symfony\Component\Config\Definition\Exception\Exception;
+use Throwable;
 
 class IdVaultService
 {
 
-    /**
-     * @var CommonGroundService
-     */
-    private $commonGroundService;
+    private $idVault;
 
-    public function __construct(
-        CommonGroundService $commonGroundService
-    ) {
-        $this->commonGroundService = $commonGroundService;
-    }
-
-    // getScope
-
-    // getClaim
-
-    /**
-     * This function returns an authorization id
-     *
-     * @param string $userUrl cleanUrl of the user
-     *
-     * @return string authorization id
-     */
-    public function getAuthorization(string $userUrl, string $resource, string $name, ?string $description)
-    {
-
+    public function __construct() {
+        $this->idVault = new IdVaultApiClient();
     }
 
     /**
-     * This function add a dossier to a current user.
+     * This function add a dossier to an id-vault user.
      *
-     * @param array $scopes scopes the dossier is blocking.
-     * @param string $authorization authorization id.
+     * @param array $scopes scopes the dossier is blocking (scopes must be authorized by the user).
+     * @param string $accessToken accessToken received from id-vault.
+     * @param string $name name of the dossier.
+     * @param string $goal the goal of the Dossier.
+     * @param string $expiryDate Expiry date of the Dossier (example: "27-10-2020 12:00:00").
+     * @param string $sso valid URL with which the user can view this Dossier.
+     * @param string $description (optional) description of the dossier.
+     * @param bool $legal (default = false) whether or not this Dossier is on legal basis.
      *
-     * @return boolean true if the dossier created was successful, false otherwise.
+     * @return array|string response from id-vault if dossier created was successful, error message otherwise.
      */
-    public function createDossier(array $scopes, string $authorization, string $name, ?string $description)
+    public function createDossier(array $scopes, string $accessToken, string $name, string $goal, string $expiryDate, string $sso, string $description = '', bool $legal = false)
     {
-        try {
-            $headers = [
-                'authentication' => 'Bearer test_H8PeFq62HpNFPQmer4GuEUWupMwSqQ',
-                'Accept'        => 'application/json',
-            ];
+        $result = $this->idVault->createDossier($scopes, $accessToken, $name, $goal, $expiryDate, $sso, $description, $legal);
 
-            $client = new Client([
-                // Base URI is used with relative requests
-                'base_uri' => 'https://api.mollie.com',
-                // You can set any number of default request options.
-                'timeout'  => 2.0,
-            ]);
-
-            $response = $client->request('GET', '/v2/payments/'.$id, [
-                'headers' => $headers,
-            ]);
-
-        } catch (\Throwable $e) {
-            return false;
-        }
-        return true;
+        return $result;
     }
 
     /**
@@ -83,40 +51,41 @@ class IdVaultService
      */
     public function sendMail(string $applicationId, string $body, string $subject, string $receiver, string $sender)
     {
-        try {
-            $headers = [
-                'Content-Type' => 'application/json',
-                'Accept'       => 'application/json'
-            ];
+        $result = $this->idVault->sendMail($applicationId, $body, $subject, $receiver, $sender);
 
-            $body = [
-                'applicationId' => $applicationId,
-                'body'          => $body,
-                'subject'       => $subject,
-                'receiver'      => $receiver,
-                'sender'        => $sender,
-            ];
-
-            $client = new Client([
-                // Base URI is used with relative requests
-                'headers'  => $headers,
-                'base_uri' => 'https://id-vault.com',
-                // You can set any number of default request options.
-                'timeout'  => 2.0,
-            ]);
-
-            $response = $client->request('POST', '/api/mails', [
-                'json'         => $body,
-            ]);
-
-            $response = json_decode($response->getBody()->getContents(), true);
-
-        } catch (\Throwable $e) {
-            return false;
-        }
-        return $response;
+        return $result;
     }
 
-    // createContract
+    /**
+     * this function requests additional scopes from user that they must authorize.
+     *
+     * @param array $scopes scopes you wish to request from the user.
+     * @param string $accessToken accessToken received from id-vault.
+     *
+     * @return array|Throwable returns response from id-vault
+     */
+    public function getScopes(array $scopes, string $accessToken)
+    {
+        $result = $this->idVault->getScopes($scopes, $accessToken);
+
+        return $result;
+    }
+
+    /**
+     * This function retrieve's user information from id-vault.
+     *
+     * @param string $applicationId id of your id-vault application.
+     * @param string $secret secret of your id-vault application.
+     * @param string $code the code received by id-vault oauth endpoint.
+     * @param string $state (optional) A random string used by your application to identify a unique session
+     *
+     * @return array|false returns response from id-vault or false
+     */
+    public function authenticateUser(string $code, string $applicationId, string $secret, string $state = null)
+    {
+        $result = $this->idVault->authenticateUser($code, $applicationId, $secret, $state);
+
+        return $result;
+    }
 
 }
